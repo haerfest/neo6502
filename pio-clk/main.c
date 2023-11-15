d2afac3ebdef00d6865146296b6ebeca9ba0746d
haerfest <wouter.hobers@gmail.com>
2023-11-15 20:00:42 +0100

    First stab at PIO.

---
+++ pio-clk/main.c
@@ -0,0 +1,18 @@
+/**
+ * main.c
+ *
+ * Does nothing yet.
+ */
+
+
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+
+
+int main() {
+  while (true) {
+    sleep_ms(1000);
+  }
+
+  __builtin_unreachable();
+}
