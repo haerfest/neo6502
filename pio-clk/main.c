63c5592b27b11127114e5e66fc254bb0aab8e377
haerfest <wouter.hobers@gmail.com>
2023-11-16 20:54:32 +0100

    It compiles.

--- pio-clk/main.c
+++ pio-clk/main.c
@@ -5,13 +5,33 @@
  */
 
 
+#include "hardware/clocks.h"
 #include "hardware/gpio.h"
+#include "hardware/pio.h"
 #include "pico/stdlib.h"
 
 
+#include "clk6502.pio.h"
+
+
 int main() {
+  stdio_init_all();
+
+  PIO  pio    = pio0;
+  uint offset = pio_add_program(pio, &clk6502_program);
+  uint sm     = pio_claim_unused_sm(pio, true);
+
+  clk6502_program_init(pio, sm, offset);
+
+  uint request;
   while (true) {
-    sleep_ms(1000);
+    request = pio_sm_get_blocking(pio, sm);
+    if (request & 1 == 0) {
+      // Write request, ignore.
+    } else {
+      // Read request, return NOP.
+      pio_sm_put_blocking(pio, sm, 0xEA);
+    }
   }
 
   __builtin_unreachable();
