985f6f7cebf414dcf864ac47792880aa6231a300
haerfest <wouter.hobers@gmail.com>
2023-11-17 18:34:18 +0100

    Intermediate commit.

--- pio-clk/main.c
+++ pio-clk/main.c
@@ -8,13 +8,27 @@
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
 #include "pico/stdlib.h"
-
-
 #include "clk6502.pio.h"
+
+
+#define PIN_AUDIO 20
 
 
 int main() {
   stdio_init_all();
+
+  sleep_ms(5000);
+
+  gpio_init(PIN_AUDIO);
+  gpio_set_dir(PIN_AUDIO, GPIO_OUT);
+  gpio_put(PIN_AUDIO, 0);
+
+  for (int i = 0; i < 50; i++) {
+    gpio_put(PIN_AUDIO, 1);
+    sleep_ms(5);
+    gpio_put(PIN_AUDIO, 0);
+    sleep_ms(5);
+  }
 
   PIO  pio    = pio0;
   uint offset = pio_add_program(pio, &clk6502_program);
@@ -22,9 +36,7 @@ int main() {
 
   clk6502_program_init(pio, sm, offset);
 
-  // Take the 6502 out of reset after at least two clock ticks
-  // or 1 us.  Let's wait a little longer.
-  sleep_us(5);
+  // Take the 6502 out of reset.
   gpio_put(PIN_RESB, 1);
 
   while (true) {
@@ -37,7 +49,11 @@ int main() {
     const uint16_t address = (request & 0x1FFFE00) >> 9;
 
     if (do_read) {
-      pio_sm_put_blocking(pio, sm, 0xEA);
+      // RESET vector points to 0x1234, otherwise return NOP (0xEA).
+      const uint8_t response =
+        address == 0xFFFC ? 0x34 :
+        address == 0xFFFD ? 0x12 : 0xEA;
+      pio_sm_put_blocking(pio, sm, response);
     }
   }
 
